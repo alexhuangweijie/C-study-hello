fcfa494c410c0648abe6c9eb09a8c4d564be20b1
alexhuangweijie <alexhuangweijie@gmail.com>
2020-01-09 16:51:30 +0800

    matrix study

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 using namespace Eigen;
 using namespace std;
-int main() {
+/*int main() {
     MatrixXd m = MatrixXd::Random(3,3);
     m = (m + MatrixXd::Constant(3,3,1.2)) * 50;
     cout << "m =" << endl << m << endl;
@@ -12,4 +12,28 @@ int main() {
     cout << "m * v =" << endl << m * v << endl;
     std::cout << "Hello, World!" << std::endl;
     return 0;
-}
+}*/
+/*
+ int main(){
+     for (int ix = 0; ix<10;++ix) {
+         cout << ix << '\n';
+     }
+     cout<<"change"<<'\n';
+     for (int iy = 0; iy<10;iy++) {
+         cout << iy << '\n';
+     }
+     return 0;
+ }*/
+
+int main (){
+    Matrix<double, 3,3> A;
+    A<<1,2,3,
+       4,5,6,
+       7,8,9;
+    cout << A << '\n'<< endl;
+    A<<A,A;
+    cout<< A <<endl;
+return 0;
+};
+
+

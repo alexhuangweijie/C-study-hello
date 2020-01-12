e44ecd6645b57e68b7897b86245efbed9d72098b
alexhuangweijie <alexhuangweijie@gmail.com>
2020-01-12 14:42:20 +0800

    myProduct function

--- main.cpp
+++ main.cpp
@@ -38,26 +38,30 @@ int main (){
 return 0;
 };
 */
+MatrixXd myproduct (MatrixXd b,MatrixXd c,int m1,int m2) {
+    /*this the a prodduct function used in dmrg algorithm
+     * which will do kronecker product of two identity matrix of size m1 and m2 with matrix B
+     * then the result will do matrix product with c
+     * return a matrix of size (m1*b.rows*m2.1)
+     * same as the size of c
+     * */
 
-
-
-
-MatrixXd myproduct (MatrixXd a,MatrixXd b,MatrixXd c,int m1,int m2){
-    for (int index1_1 = 1;index1_1<=m1;++index1_1){
-        for (int index1_2 = 1;index1_2<=b.rows();++index1_2){
-            for (int index1_3 = 1;index1_3<=m2;++index1_3){
-                for(int index1_4 = 1;index1_4<=b.rows();++index1_4){
-                    int temp_index1 = (index1_1-1) *b.rows()*m2+(index1_2-1)*m2+index1_3-1;
-                    int temp_index2 =(index1_1-1)*b.rows()*m2 + (index1_4-1)*m2 + index1_3-1;
+    MatrixXd a(c.rows(),c.cols());
+    for (int index1_1 = 1; index1_1 <= m1; ++index1_1) {
+        for (int index1_2 = 1; index1_2 <= b.rows(); ++index1_2) {
+            for (int index1_3 = 1; index1_3 <= m2; ++index1_3) {
+                for (int index1_4 = 1; index1_4 <= b.rows(); ++index1_4) {
+                    int temp_index1 = (index1_1 - 1) * b.rows() * m2 + (index1_2 - 1) * m2 + index1_3 - 1;
+                    int temp_index2 = (index1_1 - 1) * b.rows() * m2 + (index1_4 - 1) * m2 + index1_3 - 1;
+                    //cout<<temp_index1<<endl;
                     //cout<<temp_index2<<endl;
-                    //cout<<temp_index2<<endl;
-                    a(temp_index1,0) = a(temp_index1,0)+b(index1_2-1,index1_4-1)*c(temp_index2,0);
-                    //cout<<c(temp_index2,0);
+                    a(temp_index1, 0) = a(temp_index1, 0) + b(index1_2 - 1, index1_4 - 1) * c(temp_index2, 0);
+                    //cout<<index1_4-1;
                 }
             }
         }
     }
-return a;
+    return a;
 }
 int main(){
     MatrixXd A(27,1);
@@ -87,10 +91,7 @@ int main(){
     C(1,0)=temp;
     cout<<temp;
     cout<<C(temp,0);*/
-    MatrixXd E = myproduct(A,B,C,_size_m1,_size_m2);
+    MatrixXd E = myproduct(B,C,_size_m1,_size_m2);
     cout<<E;
     return 0;
 }
-
-
-
